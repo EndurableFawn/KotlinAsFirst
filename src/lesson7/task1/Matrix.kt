dd9c2e83de7eb71920cba7c3af33fecd827e3998
EndurableFawn <555bobe555@gmail.com>
2017-11-19 21:41:53 +0300

    Solve lesson7.task1

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -1,4 +1,5 @@
 @file:Suppress("UNUSED_PARAMETER", "unused")
+
 package lesson7.task1
 
 /**
@@ -21,6 +22,7 @@ interface Matrix<E> {
      * Методы могут бросить исключение, если ячейка не существует или пуста
      */
     operator fun get(row: Int, column: Int): E
+
     operator fun get(cell: Cell): E
 
     /**
@@ -28,6 +30,7 @@ interface Matrix<E> {
      * Методы могут бросить исключение, если ячейка не существует
      */
     operator fun set(row: Int, column: Int, value: E)
+
     operator fun set(cell: Cell, value: E)
 }
 
@@ -38,32 +41,64 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = MatrixImpl(height, width, e)
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : Matrix<E> {
+    private val map = mutableMapOf<Cell, E>()
 
-    override val width: Int = TODO()
+    init {
+        for (i in 0 until height) {
+            for (j in 0 until width) {
+                map[Cell(i, j)] = e
+            }
+        }
+    }
 
-    override fun get(row: Int, column: Int): E  = TODO()
+    override fun get(row: Int, column: Int): E = get(Cell(row, column))
 
-    override fun get(cell: Cell): E  = TODO()
+    override fun get(cell: Cell): E {
+        if (map[cell] == null) throw IndexOutOfBoundsException()
+        return map[cell]!!
+    }
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        set(Cell(row, column), value)
     }
 
     override fun set(cell: Cell, value: E) {
-        TODO()
+        map[cell] = value
     }
 
-    override fun equals(other: Any?) = TODO()
+    override fun equals(other: Any?) =
+            other is MatrixImpl<*> &&
+                    map == other.map
 
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append("[")
+        for (row in 0 until height) {
+            sb.append("[")
+            for (column in 0 until width - 1) {
+                sb.append(map[Cell(row, column)])
+                sb.append(", ")
+            }
+            sb.append(map[Cell(row, width - 1)])
+            sb.append("]")
+        }
+        sb.append("]")
+        return "$sb"
+    }
+
+    override fun hashCode(): Int {
+        var result = height
+        result = 31 * result + width
+        result = 31 * result + map.hashCode()
+        return result
+    }
 }
 
